28ed5b439991aa860fd52884584f916ac9aa4877
Mayowa Aj <mayowaaj@gmail.com>
2026-01-02 14:43:55 +0100

    refactor: - extracts telegram notifications into a notifier class - code formatting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::{env, fs};
-use serde::{Serialize, Deserialize};
-use teloxide::{Bot, prelude::Requester};
 use dotenv::dotenv;
-use tokio::sync::mpsc;
+use serde::{Deserialize, Serialize};
+use std::{env, fs};
+use teloxide::{Bot, prelude::Requester};
+use tokio::sync::mpsc::{self, Sender};
 
-
-#[derive(Serialize, Deserialize , Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Service {
     host: String,
     service_type: ServiceType,
@@ -13,7 +12,7 @@ struct Service {
     enabled: bool,
 }
 
-#[derive(Serialize, Deserialize , Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct ServiceStatus {
     service: Service,
     is_up: bool,
@@ -30,51 +29,88 @@ struct AppConfig {
     services: Vec<Service>,
 }
 
+struct Gossip {}
+
+struct Scheduler {}
+
+struct TelegramNotifier {
+    telegram_sender_channel: Sender<ServiceStatus>,
+}
+
+impl TelegramNotifier {
+    pub fn new() -> TelegramNotifier {
+        let (bot_sender_channel, mut bot_receiver_channel) = mpsc::channel::<ServiceStatus>(32);
+
+        tokio::spawn(async move {
+            let bot = Bot::from_env();
+            let chat_id = env::var("CHAT_ID")
+                .expect("CHAT_ID must be set")
+                .parse::<i64>()
+                .expect("CHAT_ID must be a valid i64");
+            let receiver_handle = tokio::spawn(async move {
+                while let Some(service_status) = bot_receiver_channel.recv().await {
+                    if let Err(e) = bot
+                        .send_message(
+                            teloxide::types::ChatId(chat_id),
+                            service_status.service.name.clone()
+                                + if service_status.is_up {
+                                    " is UP"
+                                } else {
+                                    " is DOWN"
+                                },
+                        )
+                        .await
+                    {
+                        eprintln!("Failed to send message: {:?}", e);
+                    }
+                }
+            });
+
+            if let Err(e) = receiver_handle.await {
+                eprintln!("Receiver task panicked: {:?}", e);
+            }
+        });
+
+        TelegramNotifier {
+            telegram_sender_channel: bot_sender_channel,
+        }
+
+    }
+
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok(); 
+    dotenv().ok();
 
-    let bot = Bot::from_env();
-    let chat_id = env::var("CHAT_ID").expect("CHAT_ID must be set").parse::<i64>().expect("CHAT_ID must be a valid i64");
-
-
-    let app_config_yaml: String = fs::read_to_string("config/services.yml").expect("Failed to read config/app.yml");
+    let app_config_yaml: String =
+        fs::read_to_string("config/services.yml").expect("Failed to read config/app.yml");
     let app_config: AppConfig = serde_yaml::from_str(&app_config_yaml)?;
 
+    let telegram_notifier = TelegramNotifier::new();
 
-
-
-    // We'll spawn Tokio tasks instead of OS threads and use a Tokio channel for async message passing
     let mut ping_tasks = Vec::new();
 
-    let (bot_sender_channel, mut bot_receiver_channel) = mpsc::channel::<ServiceStatus>(32);
+    for service in app_config
+        .services
+        .into_iter()
+        .filter(|service| service.enabled)
+    {
+        // let bot_sender_channel_clone = bot_sender_channel.clone();
+        let bot_sender_channel_clone = telegram_notifier.telegram_sender_channel.clone();
 
-    let receiver_handle = tokio::spawn(async move {
-        while let Some(service_status) = bot_receiver_channel.recv().await {
-            if let Err(e) = bot.send_message(
-                teloxide::types::ChatId(chat_id),
-                service_status.service.name.clone() + if service_status.is_up { " is UP" } else { " is DOWN" }
-            ).await {
-                eprintln!("Failed to send message: {:?}", e);
-            }
-        }
-    });
-
-    for service in app_config.services.into_iter().filter(|service| service.enabled) {
-        let bot_sender_channel_clone = bot_sender_channel.clone();
         let ping_task = tokio::spawn(async move {
             match service.service_type {
                 ServiceType::Http => {
                     // perform blocking HTTP request in a blocking task
                     handle_http_service(service, bot_sender_channel_clone).await;
-                },
+                }
                 _ => {
                     println!("Unsupported service type for service: {}", service.name);
                 }
             };
         });
         ping_tasks.push(ping_task);
-
     }
 
     // wait for all ping tasks to finish and then the receiver
@@ -84,12 +120,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    drop(bot_sender_channel); // Close the sender to signal the receiver to finish
+    drop(telegram_notifier.telegram_sender_channel); // Close the sender to signal the receiver to finish
 
-    if let Err(e) = receiver_handle.await {
-        eprintln!("Receiver task panicked: {:?}", e);
-    }
-   
+
     Ok(())
 }
 
@@ -100,25 +133,26 @@ async fn handle_http_service(service: Service, bot_sender: mpsc::Sender<ServiceS
 
     let res = reqwest::get(host.as_str()).await;
 
-
     match res {
         Ok(_resp) => {
             println!("{} is UP", name);
-            let service_status = ServiceStatus { service: service.clone(), is_up: true };
-            if bot_sender.send(service_status).await.is_err() {
-                eprintln!("Receiver dropped when sending UP for {}", name);
-            }
-        },
-        _ => {
-            println!("{} is DOWN", name);
-            let service_status = ServiceStatus { service: service.clone(), is_up: true };
+            let service_status = ServiceStatus {
+                service: service.clone(),
+                is_up: true,
+            };
             if bot_sender.send(service_status).await.is_err() {
                 eprintln!("Receiver dropped when sending UP for {}", name);
             }
         }
-
-        
+        _ => {
+            println!("{} is DOWN", name);
+            let service_status = ServiceStatus {
+                service: service.clone(),
+                is_up: true,
+            };
+            if bot_sender.send(service_status).await.is_err() {
+                eprintln!("Receiver dropped when sending UP for {}", name);
+            }
+        }
     }
-
-    
 }
