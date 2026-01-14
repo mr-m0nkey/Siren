6f6614412ca336e06b550dcec1713d26b06ee630
Mayowa Aj <mayowaaj@gmail.com>
2026-01-14 14:45:49 +0100

    Calls services at a configuable interval

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use dotenv::dotenv;
+use gossip::heartbeat_server::{Heartbeat, HeartbeatServer};
 use serde::{Deserialize, Serialize};
-use std::{env, fs};
+use std::{env, fs, time::Duration};
 use teloxide::{Bot, prelude::Requester};
 use tokio::sync::mpsc::{self, Sender};
-use gossip::heartbeat_server::{Heartbeat, HeartbeatServer};
-
 
 pub mod gossip {
     tonic::include_proto!("gossip");
@@ -16,6 +15,7 @@ struct Service {
     service_type: ServiceType,
     name: String,
     enabled: bool,
+    interval: u64
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -53,7 +53,7 @@ impl TelegramNotifier {
                 .expect("CHAT_ID must be set")
                 .parse::<i64>()
                 .expect("CHAT_ID must be a valid i64");
-            let receiver_handle = tokio::spawn(async move {
+            let receiver_handle: tokio::task::JoinHandle<()> = tokio::spawn(async move {
                 while let Some(service_status) = bot_receiver_channel.recv().await {
                     if let Err(e) = bot
                         .send_message(
@@ -66,7 +66,7 @@ impl TelegramNotifier {
                                 },
                         )
                         .await
-                    {
+                    { 
                         eprintln!("Failed to send message: {:?}", e);
                     }
                 }
@@ -80,21 +80,14 @@ impl TelegramNotifier {
         TelegramNotifier {
             telegram_sender_channel: bot_sender_channel,
         }
-
     }
-
 }
-
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
     // tonic_build::compile_protos("proto/helloworld.proto")?;
-
-
-
 
     let app_config_yaml: String =
         fs::read_to_string("config/services.yml").expect("Failed to read config/app.yml");
@@ -135,7 +128,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     drop(telegram_notifier.telegram_sender_channel); // Close the sender to signal the receiver to finish
 
-
     Ok(())
 }
 
@@ -143,32 +135,42 @@ async fn handle_http_service(service: Service, bot_sender: mpsc::Sender<ServiceS
     println!("Pinging HTTP service: {}", service.name);
     let name = service.name.clone();
     let host = service.host.clone();
+    let mut previous_is_up = false;
+    let mut first_run = true;
 
-    let res = reqwest::get(host.as_str()).await;
+    loop {
+        let res = reqwest::get(host.as_str()).await;
 
-    match res {
-        Ok(_resp) => {
-            println!("{} is UP", name);
-            let service_status = ServiceStatus {
-                service: service.clone(),
-                is_up: true,
-            };
-            if bot_sender.send(service_status).await.is_err() {
-                eprintln!("Receiver dropped when sending UP for {}", name);
+        match res {
+            Ok(_resp) => {
+                println!("{} is UP", name);
+                let service_status = ServiceStatus {
+                    service: service.clone(),
+                    is_up: true,
+                };
+                if (first_run || previous_is_up != true)
+                    && bot_sender.send(service_status).await.is_err()
+                {
+                    eprintln!("Receiver dropped when sending UP for {}", name);
+                }
+                previous_is_up = true;
+                first_run = false;
+            }
+            _ => {
+                println!("{} is DOWN", name);
+                let service_status = ServiceStatus {
+                    service: service.clone(),
+                    is_up: false,
+                };
+                if (first_run || previous_is_up != false)
+                    && bot_sender.send(service_status).await.is_err()
+                {
+                    eprintln!("Receiver dropped when sending UP for {}", name);
+                }
+                previous_is_up = false;
+                first_run = false;
             }
         }
-        _ => {
-            println!("{} is DOWN", name);
-            let service_status = ServiceStatus {
-                service: service.clone(),
-                is_up: true,
-            };
-            if bot_sender.send(service_status).await.is_err() {
-                eprintln!("Receiver dropped when sending UP for {}", name);
-            }
-        }
+        tokio::time::sleep(Duration::from_secs(service.interval)).await;
     }
 }
-
-
-
